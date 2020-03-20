fdcf4179f4160c634381cba44e14dce1435bb216
fulder <misad90@gmail.com>
2020-03-20 18:43:21 +0100

    Move MAC-IP mapping to own function and return dict

--- mac_dns.py
+++ mac_dns.py
@@ -9,6 +9,14 @@ from scapy.sendrecv import srp
 def main():
     conf = _read_config()
 
+
+def _read_config():
+    with open("mac_dns.yaml", 'r') as fs:
+        return yaml.safe_load(fs)
+
+
+def _get_mac_ip_mapping():
+    mapping = {}
     ips = ipaddress.ip_network('192.168.1.0/30')
     for ip in ips:
         broadcast = Ether(dst="FF:FF:FF:FF:FF:FF")
@@ -20,12 +28,8 @@ def main():
             if rcv:
                 ip = rcv[ARP].psrc
                 mac = rcv[Ether].src
-                print(f"{ip} - {mac}")
-
-
-def _read_config():
-    with open("mac_dns.yaml", 'r') as fs:
-        return yaml.safe_load(fs)
+                mapping[mac] = ip
+    return mapping
 
 
 if __name__ == "main":
