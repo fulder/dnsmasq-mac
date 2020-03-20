07259671a065308cf1c2066e9c827fee392bcc8e
fulder <misad90@gmail.com>
2020-03-20 18:40:19 +0100

    Add main function and check

--- mac_dns.py
+++ mac_dns.py
@@ -1,18 +1,24 @@
 import ipaddress
+import sys
 
 from scapy.layers.l2 import ARP, Ether
 from scapy.sendrecv import srp
 
 
-ips = ipaddress.ip_network('192.168.1.0/30')
-for ip in ips:
-    broadcast = Ether(dst="FF:FF:FF:FF:FF:FF")
-    arp_request = ARP(pdst=str(ip))
-    package = broadcast / arp_request
-    ans, uans = srp(package, iface="enp0s31f6", timeout=0.1, verbose=False)
+def main():
+    ips = ipaddress.ip_network('192.168.1.0/30')
+    for ip in ips:
+        broadcast = Ether(dst="FF:FF:FF:FF:FF:FF")
+        arp_request = ARP(pdst=str(ip))
+        package = broadcast / arp_request
+        ans, uans = srp(package, iface="eth0", timeout=0.1, verbose=False)
 
-    for snd, rcv in ans:
-        if rcv:
-            ip = rcv[ARP].psrc
-            mac = rcv[Ether].src
-            print(f"{ip} - {mac}")
+        for snd, rcv in ans:
+            if rcv:
+                ip = rcv[ARP].psrc
+                mac = rcv[Ether].src
+                print(f"{ip} - {mac}")
+
+
+if __name__ == "main":
+    sys.exit(main())
