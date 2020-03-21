9ea96635d48a850abbc8a38876776ce161ff57a5
fulder <misad90@gmail.com>
2020-03-21 16:30:50 +0100

    Ignore not found names as hosts can be down

--- mac_dns.py
+++ mac_dns.py
@@ -85,8 +85,7 @@ def _get_mac_ip_mapping(ips: list, interface: str, mac_names: dict):
         count += 1
 
     if len(ip_dns_mapping) != len(mac_names):
-        logger.warning(f"Not found names:{set(mac_names.values()) - set(ip_dns_mapping.values())}")
-        raise Exception("Couldn't find all specified MAC addresses, make sure the range and all ARNs are correct")
+        logger.warning(f"All names not found, ignoring:{set(mac_names.values()) - set(ip_dns_mapping.values())}")
     return ip_dns_mapping
 
 
